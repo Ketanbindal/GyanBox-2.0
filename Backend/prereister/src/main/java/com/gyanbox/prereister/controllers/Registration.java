6fc11de3da2c35f187bb04e6e29223bd9184575a
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-03-08 22:50:22 +0530

    Update message

--- Backend/prereister/src/main/java/com/gyanbox/prereister/controllers/Registration.java
+++ Backend/prereister/src/main/java/com/gyanbox/prereister/controllers/Registration.java
@@ -1,16 +1,28 @@
 package com.gyanbox.prereister.controllers;
 
 
+import com.gyanbox.prereister.Models.User;
+import com.gyanbox.prereister.controllers.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Registration {
 
-    @PostMapping("/preregister")
-    public String preregister(){
 
-        return "Y";
+//    @Autowired
+//    User user;
+
+    @Autowired
+    UserRepository userRepository;
+    @PostMapping("/preregister")
+    public String preregister(@RequestBody User user){
+
+        userRepository.save(user);
+        return "User preregistered successfully";
     }
 
 }
